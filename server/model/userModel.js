bac1a420f7c829beb36f4b178318df01e04beb50
yash-199 <68182949+yash-199@users.noreply.github.com>
2024-10-27 16:21:29 +0530

    webhooks development

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    clerkId: { trype: String, required: true, unique: true },
+    clerkId: { type: String, required: true, unique: true }, // Corrected `type`
     email: { type: String, required: true, unique: true },
     photo: { type: String, required: true },
     firstName: { type: String },
     lastName: { type: String },
     creditBalance: { type: Number, default: 5 }
-})
+});
 
-
-const userModel = mongoose.models.user || mongoose.model("user", userSchema)
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 
 export default userModel;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 // App Config
 const PORT = process.env.PORT || 4000
@@ -14,5 +15,6 @@ app.use(cors())
 
 // API routes
 app.get('/', (req, res) => res.send("API Working"))
+app.use('/api/user', userRouter)
 
 app.listen(PORT, () => console.log("Server Running on port" + PORT))
